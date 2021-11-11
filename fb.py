1bc90ecebe7c3f0cd5cb0c1a17d3f1096d0d5a76
ushabisht21 <93266927+ushabisht21@users.noreply.github.com>
2021-11-11 09:56:17 +0530

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,28 @@
+# num=input("enter the name")
+# if num>="a to z ":
+#     print("name")
+#     birth=(input("enter the date of birth"))
+#     if birth >="1 to 9" :
+#      print("date")
+#     number=(input("enter the phone number"))
+#     if number >="1 to 9 ":
+#         print (" varify your phone number")
+#         gender =input("enter the gender")
+#         if gender=="male" or gender=="female":
+#             print(gender)
+#             password=(input("enter the password"))
+#             if password==100000 or "letter":
+#                  print("password")
+#             code=int(input("enter your 6 digit code"))
+#             if code >=100000:
+#                 print("code")
+#                 facebook=input("welcome to facebook ")
+#                 print("welcome to facebook")
+#             else:
+#                 print("invelid same")
+#         else:
+            # print("something  worng")  
+#     else:
+#         print("nothing")                  
+# else:
+#     print("noo")
