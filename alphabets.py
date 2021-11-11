533d999a2b0d047db8f21509fb3f2e518c07b302
ushabisht21 <93266927+ushabisht21@users.noreply.github.com>
2021-11-11 10:29:01 +0530

    Add files via upload

---
+++ alphabets.py
@@ -0,0 +1,6 @@
+ab=input("enter the alphabet")
+if (ab >='a' and ab<='z') or (ab>='A'and ab <='Z'):
+    print(ab,"is an alphabet")
+else:
+    print(ab,"is not an alphabet")    
+
---
+++ vowel.py
@@ -0,0 +1,7 @@
+num=input("enter the vowel:")
+a="aeiouAEIOU"
+if num in a:
+    print(num,"vowel")
+else:
+    print(num,"consonent")
+  
